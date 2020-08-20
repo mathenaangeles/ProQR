4c95684e33e91abdb5e26015dc2064226bbe9cc2
limlance99 <limlance_99@yahoo.com>
2020-08-20 21:10:43 +0800

    fixed wrong spelling of constants

---
+++ src/pages/Listings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>hello</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Listings",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+h1 {
+    color: blue,
+}
+</style>
